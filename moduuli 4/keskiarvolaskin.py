34aecf00a600b00554af9355470dfc02f712d6af
UmaTheDoggo <leevilucas@gmail.com>
2026-08-21 13:58:59 +0300

    muutoksia tehtävään 4

--- moduuli 4/keskiarvolaskin.py
+++ moduuli 4/keskiarvolaskin.py
@@ -1,12 +1,12 @@
-Kokonaisluku1 = float(input("Syötä kokonaisluku: "))
-Kokonaisluku2 = float(input("Syötä kokonaisluku: "))
-Kokonaisluku3 = float(input("Syötä kokonaisluku: "))
+Kokonaisluku1 = int(input("Syötä kokonaisluku: "))
+Kokonaisluku2 = int(input("Syötä kokonaisluku: "))
+Kokonaisluku3 = int(input("Syötä kokonaisluku: "))
 
 #summa = Kokonaisluku1 + Kokonaisluku2 + Kokonaisluku3
-print(f"Summa on {Kokonaisluku1 + Kokonaisluku2 + Kokonaisluku3:.2f}")
+print(f"Summa on {Kokonaisluku1 + Kokonaisluku2 + Kokonaisluku3}")
 
 #tulo = Kokonaisluku1 * Kokonaisluku2 * Kokonaisluku3
-print(f"Tulo on {Kokonaisluku1 * Kokonaisluku2 * Kokonaisluku3:.2f}")
+print(f"Tulo on {Kokonaisluku1 * Kokonaisluku2 * Kokonaisluku3}")
 
 #keskiarvo = (Kokonaisluku1 + Kokonaisluku2 + Kokonaisluku3) / 3
-print(f"Keskiarvo on {(Kokonaisluku1 + Kokonaisluku2 + Kokonaisluku3) / 3:.2f}")
+print(f"Keskiarvo on {(Kokonaisluku1 + Kokonaisluku2 + Kokonaisluku3) / 3}")
