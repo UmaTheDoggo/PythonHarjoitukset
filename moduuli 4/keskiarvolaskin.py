3143c7b7d04c9ef0fa33112024114730094b9a68
UmaTheDoggo <leevilucas@gmail.com>
2026-08-21 13:56:21 +0300

    Tehtävä 4 tehty

---
+++ moduuli 4/keskiarvolaskin.py
@@ -0,0 +1,12 @@
+Kokonaisluku1 = float(input("Syötä kokonaisluku: "))
+Kokonaisluku2 = float(input("Syötä kokonaisluku: "))
+Kokonaisluku3 = float(input("Syötä kokonaisluku: "))
+
+#summa = Kokonaisluku1 + Kokonaisluku2 + Kokonaisluku3
+print(f"Summa on {Kokonaisluku1 + Kokonaisluku2 + Kokonaisluku3:.2f}")
+
+#tulo = Kokonaisluku1 * Kokonaisluku2 * Kokonaisluku3
+print(f"Tulo on {Kokonaisluku1 * Kokonaisluku2 * Kokonaisluku3:.2f}")
+
+#keskiarvo = (Kokonaisluku1 + Kokonaisluku2 + Kokonaisluku3) / 3
+print(f"Keskiarvo on {(Kokonaisluku1 + Kokonaisluku2 + Kokonaisluku3) / 3:.2f}")
