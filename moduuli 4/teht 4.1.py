2058c84113c56102f03629febde12c405da2be4f
UmaTheDoggo <leevilucas@gmail.com>
2026-08-27 10:56:10 +0300

    Moduuli 4

---
+++ moduuli 4/teht 4.1.py
@@ -0,0 +1,5 @@
+pituus = float(input("Anna pituus senttimetreinä:"))
+if pituus < 37:
+    print("Päästä kala takaisin järveen")
+else:
+    print("Voitte syödä kalan :)")
---
+++ moduuli 4/teht 4.2.py
@@ -0,0 +1,16 @@
+hyttiluokka = input("Anna hyttiluokka (LUX, A, B, C): ")
+
+if hyttiluokka in ["LUX", "lux", "Lux"]:
+    print("LUX on parvekkeellinen hytti yläkannella")
+
+elif hyttiluokka in ["A", "a"]:
+    print("A on ikkunallinen hytti autokannen yläpuolella")
+
+elif hyttiluokka in ["B", "b"]:
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+
+elif hyttiluokka in ["C", "c"]:
+    print("C on ikkunaton hytti autokannen alapuolella")
+    
+else:
+    print("Virheellinen hyttiluokka.")
