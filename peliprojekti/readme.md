c02c0b64ff5141b1148fd853f716f563dbb4afd1
UmaTheDoggo <leevilucas@gmail.com>
2026-08-27 16:13:44 +0300

    Pelin nimi vaihdettu. Korjauksia koodiin

--- peliprojekti/readme.md
+++ peliprojekti/readme.md
@@ -1,3 +1,3 @@
-Löysäpeli
+Kauppareissu
 
 -Leevi Luukkonen
--- peliprojekti/userinfo.py
+++ peliprojekti/userinfo.py
@@ -2,7 +2,7 @@
 player_name = input("Enter your name: ")
 
 #player age
-player_age = (input("Enter your age: "))
+player_age = input("Enter your age: ")
 
 #player info
 player_info = print (f"Player name: {player_name}, Player age: {player_age}")
