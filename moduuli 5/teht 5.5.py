e09747120097e55bc01ebaefcdb43f41b6ab64d5
UmaTheDoggo <leevilucas@gmail.com>
2026-08-31 19:23:40 +0300

    tehtävä 5.5

---
+++ moduuli 5/teht 5.5.py
@@ -0,0 +1,14 @@
+for i in range (5):
+    user = input("Kirjoita käyttäjätunnus: ")
+    if user == "python":
+        password = input("Kirjoita salasana: ")
+        if password == "rules":
+            print("Tervetuloa")
+            break
+        else:
+            print("Väärä salasana")
+    else:
+        print("Väärä käyttäjätunnus")
+
+else:
+    print("Pääsy evätty")
