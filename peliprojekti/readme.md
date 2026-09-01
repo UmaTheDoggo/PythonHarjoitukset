bda58617f762b466a93152bb1aea1956c58a9a23
UmaTheDoggo <leevilucas@gmail.com>
2026-09-01 16:23:22 +0300

    peli projekti 2

--- peliprojekti/readme.md
+++ peliprojekti/readme.md
@@ -1,3 +1,3 @@
-Kauppareissu
+The Dungeon
 
 -Leevi Luukkonen
--- peliprojekti/userinfo.py
+++ peliprojekti/userinfo.py
@@ -1,8 +1,43 @@
+import sys
+import time
+
 #player name
 player_name = input("Enter your name: ")
 
 #player age
-player_age = input("Enter your age: ")
+player_age = float(input("Enter your age: "))
+if player_age < 12:
+    sys.exit("This game is meant for people over the age of 12. Quitting game.")
+
+print(" ")
 
 #player info
-player_info = print (f"Player name: {player_name}, Player age: {player_age}")
+player_info = print (f"Hello, {player_name}, Age: {player_age:.0f}")
+
+while True:
+    time.sleep(1)
+
+    print("""\
+------------------------------------------------------------------------------------------------------------------------
+__        __   _                            _          _____ _            ____                                     
+\ \      / /__| | ___ ___  _ __ ___   ___  | |_ ___   |_   _| |__   ___  |  _ \ _   _ _ __   __ _  ___  ___  _ __  
+ \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \    | | | '_ \ / _ \ | | | | | | | '_ \ / _` |/ _ \/ _ \| '_ \ 
+  \ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) |   | | | | | |  __/ | |_| | |_| | | | | (_| |  __/ (_) | | | |
+   \_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/    |_| |_| |_|\___| |____/ \__,_|_| |_|\__, |\___|\___/|_| |_|
+                                                                                            |___/                  
+------------------------------------------------------------------------------------------------------------------------
+  | Start |     | Customise |     | Info |     | Quit |                                                                          
+
+""")
+    
+    navigation = input("Type to navigate: ").lower()
+    if navigation == "info":
+        print(f"Name: {player_name}, Age {player_age:.0f}")
+
+    if navigation == "customise":
+        hat_color = input("Choose the color of the hat: ").lower()
+        jacket_color = input("Choose the color of the jacket: ").lower()
+        jeans_color = input("Choose the color of the jeans: ").lower()
+
+    if navigation == "quit":
+        sys.exit("Quitting game...")
